fcbc3d6e9b47acfad5639e74e398eb66aa54b048
Tommo J. Phillips <tommojphillips@gmail.com>
2023-04-04 18:05:25 +1000

    # implemented jerry holder mod => v1.0.457.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,21 +7,21 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tommo J. Productions")]
 [assembly: AssemblyProduct("Jerry Can Holder Mod")]
-[assembly: AssemblyCopyright("Tommo J. Productions Copyright © 2022")]
+[assembly: AssemblyCopyright("Tommo J. Productions Copyright © 2023")]
 [assembly: AssemblyTrademark("Azine")]
 [assembly: NeutralResourcesLanguage("en-AU")]
 
 // Version information
-[assembly: AssemblyVersion("1.0.162.2")]
-[assembly: AssemblyFileVersion("1.0.162.2")]
+[assembly: AssemblyVersion("1.0.457.3")]
+[assembly: AssemblyFileVersion("1.0.457.3")]
 
 namespace TommoJProductions.JerryCanHolderMod
 {
 
     public class VersionInfo
     {
-	    public const string lastestRelease = "12.06.2022 01:40 PM";
-	    public const string version = "1.0.162.2";
+	    public const string lastestRelease = "03.04.2023 07:17 PM";
+	    public const string version = "1.0.457.3";
 
         /// <summary>
         /// Represents if the mod has been complied for x64
